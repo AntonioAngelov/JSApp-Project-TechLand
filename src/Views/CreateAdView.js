35b31ca2ab82cc72b7b2bbf6144c35600cdb9f3d
antoniyapp <antoniya.partacheva@abv.bg>
2016-12-03 18:25:26 +0200

    Made the Views of ads and worked on some functionalities

--- src/Views/CreateAdView.js
+++ src/Views/CreateAdView.js
@@ -1,3 +1,53 @@
-/**
- * Created by anton on 03-Dec-16.
- */
+import React, { Component } from 'react';
+
+export default class CreateBookView extends Component {
+    render() {
+        return (
+            <form className="create-app-form" onSubmit={this.submitForm.bind(this)}>
+                <h1>Create Ad</h1>
+                <label>
+                    <div>Section:</div>
+                    <select name="section" ref={e => this.sectionField= e} >
+                        <option value="phoneAds">Phone Ad</option>
+                        <option value="pcAds">PC Ad</option>
+                        <option value="otherAds">Other Ad</option>
+                    </select>
+                </label>
+                <label>
+                    <div>Image:</div>
+                    <input type="text" name="image" required
+                           ref={e => this.imageField = e} />
+                </label>
+                <label>
+                    <div>Title:</div>
+                    <input type="text" name="title" required
+                           ref={e => this.titleField = e} />
+                </label>
+                <label>
+                    <div>Description:</div>
+                    <input type="text" name="addDescription" required
+                           ref={e => this.descriptionField = e} />
+                </label>
+                <label>
+                    <div>Phone Number:</div>
+                    <textarea name="phoneNumber" rows="10"
+                              ref={e => this.phoneNumberField = e} />
+                </label>
+                <div>
+                    <input type="submit" value="Create" />
+                </div>
+            </form>
+        );
+    }
+
+    submitForm(event) {
+        event.preventDefault();
+        this.props.onsubmit(
+            this.titleField.value,
+            this.imageField.value,
+            this.descriptionField.value,
+            this.phoneNumberField.value,
+            this.sectionField.value
+        );
+    }
+}
--- src/Views/PcAdsView.js
+++ src/Views/PcAdsView.js
@@ -1,3 +1,51 @@
-PhoneAdsView.js/**
- * Created by anton on 03-Dec-16.
- */
+import React, { Component } from 'react';
+
+export default class PcAdsView extends Component {
+    render() {
+        let pcAdsRows = this.props.pcAds.map(pcAds =>
+            <tr key={pcAds._id}>
+                <td><img src={pcAds.imageURL}></img></td>
+                <td>{pcAds.title}</td>
+                <td>{pcAds.phoneDescription}</td>
+                <td>{pcAds.phoneNumber}</td>
+            </tr>
+        );
+
+        return (
+            <div className="pcAds-view">
+                <h1>Pc Ads</h1>
+                <table className="pcAds-table">
+                    <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Phone Description</th>
+                        <th>Phone Number</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {pcAdsRows}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    /*getActions(phoneAds, userId) {
+     if (phoneAds._acl.creator === userId)
+     return (
+     <td>
+     <input type="button" value="Edit"
+     onClick={this.props.editPhoneAdClicked.bind(this, phoneAds._id)} />
+     &nbsp;
+     <input type="button" value="Delete"
+     onClick={this.props.deletePhoneAdClicked.bind(this, phoneAds._id)} />
+     </td>
+     );
+     else
+     return <td></td>;
+     }*/
+}
+
+
