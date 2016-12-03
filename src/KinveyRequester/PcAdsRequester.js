e152b2f2722a72fb99e9ec59dc4edf99c36d63d8
antoniyapp <antoniya.partacheva@abv.bg>
2016-12-03 12:25:20 +0200

    created the databases

--- src/KinveyRequester/PcAdsRequester.js
+++ src/KinveyRequester/PcAdsRequester.js
@@ -1,3 +1,6 @@
-/**
- * Created by anton on 03-Dec-16.
- */
+const baseUrl = "https://baas.kinvey.com/";
+const appKey = "kid_r1jz3egXg";
+const appSecret = "3f96351ca9324ff6a85eb54815bc5e95";
+const kinveyAppAuthHeaders = {
+    'Authorization': "Basic " + btoa(appKey + ":" + appSecret),
+};
